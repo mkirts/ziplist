3342ac5bd5003895f49b7c50713316613a4ad5c4
mkirts <70680675+mkirts@users.noreply.github.com>
2020-09-21 20:36:37 -1000

    Update ziplist.js

--- ziplist.js
+++ ziplist.js
@@ -1,21 +1,23 @@
-const first = ['a', 'b', 'c'];
-const second = [1, 2, 3];
+const firstList = ['a', 'b', 'c'];
+const secondList = [1, 2, 3];
 
 function zipList(a, b) {
-  const result = [];
+  const resultList = [];
   for (let i = 0; i < a.length; i++) {
-    result.push(a[i], b[i]);
+    resultList.push(a[i], b[i]);
   }
-  return result;
+  return resultList;
 }
 
-function zipTheEasyWay(a, b) {
+function zipListTheSimpleWay(a, b) {
   return _.flatten(_.zip(a, b));
 }
 
-console.log(zipList(first, second));
-console.log(zipTheEasyWay(first, second));
+console.log(zipList(firstList, secondList));
+console.log(zipListTheSimpleWay(firstList, secondList));
 // eslint-disable-next-line max-len
 // Attempt 1: 21:33 - Tried resolving a problem that didn't exactly exist. Had to look up methods for zipping together two arrays.
 // eslint-disable-next-line max-len
 // Attempt 2: 14:32 - Continued looking into the zip method. Wasn't too bad and I do want to try things a little bit differently before submission.
+// eslint-disable-next-line max-len
+// Attempt 3: 5:44 - Went to google to see additional samples of zipping together files. The quickest way would be to use the .push call
